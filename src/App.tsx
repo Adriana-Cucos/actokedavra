69abaa74403a43e3db4cae69e47f5657a7bd6ec0
Adriana Cucos <adriana.cucos@assist.ro>
2022-08-02 00:14:24 +0300

    Add new packages and update project's structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import './App.css';
+import './App.css'
 
-import { Actor } from './components/Actor/Actor';
+import { Actor } from './components/Actor/Actor'
 
 function App() {
-
   const actor = {
-    name: "Leonardo Dicaprio",
+    name: 'Leonardo Dicaprio',
     score: 10,
-    hobbies: "Music and dancing naked in the rain",
-    description: "He is a good guy with a thick mustache."
+    hobbies: 'Music and dancing naked in the rain',
+    description: 'He is a good guy with a thick mustache.',
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Actor {...actor} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
