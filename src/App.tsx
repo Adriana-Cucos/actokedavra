4fd4d8cc6e80dbbe2b072867f3afe43bf35c2d25
Adriana Cucos <adriana.cucos@assist.ro>
2022-08-22 19:27:03 +0300

    Create 6 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import './App.css'
 import { Footer } from 'components/Footer/Footer'
 import { Header } from 'components/Header/Header'
 import { Actor } from './components/Actor/Actor'
+import { Button } from './components/Button/Button'
+
+import { BUTTON_TYPES, ICON_POSITIONS, BUTTON_BORDERS } from 'shared/constants'
 
 import styles from './App.module.scss'
+
+import iconArrow from './assets/arrow-chevron-down.png'
+import iconPen from './assets/pen-icon.png'
+import iconDelete from './assets/delete-icon.png'
 
 function App() {
   const actor = {
@@ -18,7 +25,48 @@ function App() {
     <div className={styles.app}>
       <div className={styles.appContent}>
         <Header />
-        <Actor {...actor} />
+        <Actor {...actor} className={styles.appComponent} />
+        <Button
+          text='Sort'
+          type={BUTTON_TYPES.Full}
+          iconPosition=''
+          icon=''
+          border={BUTTON_BORDERS.Bordered}
+          className={styles.appComponent}
+        />
+        <Button text='' type={BUTTON_TYPES.Round} iconPosition='' icon='' border='' className={styles.appComponent} />
+        <Button
+          text='Read more'
+          type={BUTTON_TYPES.Expandable}
+          iconPosition={ICON_POSITIONS.Right}
+          icon={iconArrow}
+          border=''
+          className={styles.appComponent}
+        />
+        <Button
+          text='Edit'
+          type={BUTTON_TYPES.EmptyIcon}
+          iconPosition={ICON_POSITIONS.Right}
+          icon={iconPen}
+          border={BUTTON_BORDERS.Bordered}
+          className={styles.appComponent}
+        />
+        <Button
+          text='Add new actor'
+          type={BUTTON_TYPES.Main}
+          iconPosition=''
+          icon=''
+          border=''
+          className={styles.appComponent}
+        />
+        <Button
+          text='Delete'
+          type={BUTTON_TYPES.IconEmpty}
+          iconPosition={ICON_POSITIONS.Left}
+          icon={iconDelete}
+          border={BUTTON_BORDERS.BoldBoldered}
+          className={styles.appComponent}
+        />
       </div>
       <Footer />
     </div>
