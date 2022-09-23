82225350a497bd0151b3bbccf8c5e14e8a6378b3
Adriana Cucos <adriana.cucos@assist.ro>
2022-09-23 10:07:43 +0300

    Add nanoid to the project

--- src/components/Hobbies/Hobbies.jsx
+++ src/components/Hobbies/Hobbies.jsx
@@ -1,3 +1,5 @@
+import { nanoid } from 'nanoid'
+
 import cx from 'classnames'
 
 import { Hobby } from 'components/Hobby/Hobby'
@@ -7,8 +9,8 @@ import styles from './Hobbies.module.scss'
 export const Hobbies = ({ texts, className }) => {
   return (
     <div className={cx(className, styles.hobbies)}>
-      {texts.map((text, index) => (
-        <Hobby key={index} text={text} />
+      {texts.map((text) => (
+        <Hobby key={nanoid()} text={text} />
       ))}
     </div>
   )
