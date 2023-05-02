02c456c748944c78e40eb569c62365863e569d4d
Adriana Cucos <adriana.cucos@assist.ro>
2023-05-02 11:31:12 +0300

    Display a static actors list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,40 @@
 import { Footer } from 'components/Footer/Footer'
 import { Header } from 'components/Header/Header'
 import { Button } from 'components/Button/Button'
-import { ActorDetails } from 'components/ActorDetails/ActorDetails'
+import { Actors } from 'components/Actors/Actors'
 import { ActorAdd } from 'components/ActorAdd/ActorAdd'
 
 import { BUTTON_TYPES, BUTTON_BORDERS } from 'shared/constants'
-import { ACTOR } from 'data'
+import { ACTORS } from 'data'
 
 import styles from './Home.module.scss'
 
 export const Home = () => {
   return (
-    <div className={`${styles.app} app`}>
+    <div className={`${styles.homepage} app`}>
       <Header />
-      <div className={`${styles.appContent} appComponent`}>
-        <Button
-          text='Sort'
-          type={BUTTON_TYPES.Full}
-          iconPosition=''
-          icon=''
-          border={BUTTON_BORDERS.Bordered}
-          className={styles.appComponent}
-          onClickEvent={null}
-        />
-        <Button
-          text='Select'
-          type={BUTTON_TYPES.Full}
-          iconPosition=''
-          icon=''
-          border={BUTTON_BORDERS.Bordered}
-          className={styles.appComponent}
-          onClickEvent={null}
-        />
-        <ActorDetails {...ACTOR} />
+      <div className={styles.homepageContent}>
+        <div className={styles.actorActions}>
+          <Button
+            text='Sort'
+            type={BUTTON_TYPES.Full}
+            iconPosition=''
+            icon=''
+            border={BUTTON_BORDERS.Bordered}
+            className={styles.actorActionsBtn}
+            onClickEvent={null}
+          />
+          <Button
+            text='Select'
+            type={BUTTON_TYPES.Full}
+            iconPosition=''
+            icon=''
+            border={BUTTON_BORDERS.Bordered}
+            className={styles.actorActionsBtn}
+            onClickEvent={null}
+          />
+        </div>
+        <Actors actors={ACTORS} />
         <ActorAdd />
       </div>
       <Footer />
